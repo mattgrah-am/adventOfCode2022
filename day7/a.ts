570fc32965bb48f7d209e3c7d054e65703c6206a
Matt Graham <iam@mattgrah.am>
2022-12-07 22:54:40 +1100

    day 7 complete

---
+++ day7/a.ts
@@ -0,0 +1,43 @@
+import * as fs from "fs";
+const inputFile: string = "input.txt";
+
+type Dir = {
+  [key: string]: number;
+};
+
+const sanitizeData = async () => {
+  fs.readFile(inputFile, "utf8", (err, data) => {
+    const lines = data.split("\n");
+
+    const dirs: Dir = {};
+    const parserPath: string[] = [];
+
+    lines.forEach((line) => {
+      if (/\d+/.test(line)) {
+        const fileSize = +line.split(" ")[0];
+
+        const path: string[] = [];
+
+        parserPath.forEach((dir) => {
+          path.push(dir);
+
+          const dirTotal = dirs[path.join("/")] ?? 0;
+          dirs[path.join("/")] = dirTotal + fileSize;
+        });
+      } else if (/\$ cd/.test(line)) {
+        const [_, _command, param] = line.split(" ");
+
+        param === ".." ? parserPath.pop() : parserPath.push(param);
+      }
+    });
+    const response = Object.values(dirs).reduce(
+      (total, dirSize) => (dirSize <= 100000 ? total + dirSize : total),
+      0
+    );
+
+    console.log(response);
+    return response;
+  });
+};
+
+sanitizeData();
