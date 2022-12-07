1558a3c4feeb23b2bf4e8f13e590b52417d1d26b
Matt Graham <iam@mattgrah.am>
2022-12-07 21:18:21 +1100

    day 5 complete

---
+++ day5/a.ts
@@ -0,0 +1,45 @@
+import * as fs from "fs";
+const inputFile: string = "input.txt";
+
+const sanitizeData = async () => {
+  fs.readFile(inputFile, "utf8", (err, data) => {
+    const stacks = [
+      ["D", "H", "N", "Q", "T", "W", "V", "B"],
+      ["D", "W", "B"],
+      ["T", "S", "Q", "W", "J", "C"],
+      ["F", "J", "R", "N", "Z", "T", "P"],
+      ["G", "P", "V", "J", "M", "S", "T"],
+      ["B", "W", "F", "T", "N"],
+      ["B", "L", "D", "Q", "F", "H", "V", "N"],
+      ["H", "P", "F", "R"],
+      ["Z", "S", "M", "B", "L", "N", "P", "H"],
+    ];
+
+    const moves = data
+      .split("\n")
+      .map((arr) => arr.split(" "))
+      .map((arr) => {
+        const arrOfMoves: number[] = [];
+        arr.map((item) => {
+          if (Number.isInteger(+item)) {
+            arrOfMoves.push(+item);
+          }
+        });
+        return arrOfMoves;
+      });
+
+    moves.forEach((move) => {
+      const tempStack: string[] = [];
+      for (let i = 0; i < move[0]; i++) {
+        tempStack.push(...stacks[move[1] - 1].splice(-1, 1));
+      }
+      stacks[move[2] - 1].push(...tempStack);
+    });
+
+    const topStacks = stacks.map((top) => top[top.length - 1]).join("");
+    console.log(topStacks);
+    return topStacks;
+  });
+};
+
+sanitizeData();
